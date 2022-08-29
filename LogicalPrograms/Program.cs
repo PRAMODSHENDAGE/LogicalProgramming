a75810d7009ab2b8eb2fa6894a3b2a86edd22c20
KajalShendage <shendagekajal123@gmail.com>
2022-08-30 00:01:24 +0530

    [Pramod] Add: Temperature Conversion

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Welcome to Logical Programming");
 while (true)
 {
-    Console.WriteLine("Select program\n 1. fibonacci series\n 2. Perfect Number\n 3. Prime Number\n 4. Reverse Number");
+    Console.WriteLine("Select program\n 1. fibonacci series\n 2. Perfect Number\n 3. Prime Number\n 4. Reverse Number\n 5. Temperature Conversion");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -17,6 +17,10 @@ while (true)
             break;
         case 4:
             ReverseNumber.ReverseANumber();
+            break;
+        case 5:
+            TemperatureConversion temperatureconversion = new TemperatureConversion();
+            temperatureconversion.Temperatureconversion();
             break;
         default:
             Console.WriteLine("Select correct option");
---
+++ LogicalPrograms/TemperatureConversion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class TemperatureConversion
+    {
+        double c, f;
+        public void Temperatureconversion()
+        {
+            Console.WriteLine("Enter 1 for CELCIUS to FAHRENHEIT CONVERSION & Enter 2 for FAHRENHEIT to CELCIUS CONVERSION ");
+            int Temperatureconversion = Convert.ToInt32(Console.ReadLine());
+            switch (Temperatureconversion)
+            {
+                case 1:
+                    Console.WriteLine("Enter any Temperature value in celcius");
+                    c = Convert.ToDouble(Console.ReadLine());
+                    f = c * 9 / 5 + 32;
+                    Console.WriteLine("Temperature in FAHRENHEIT " + f);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter any Temperature value in Fahrenheit");
+                    f = Convert.ToDouble(Console.ReadLine());
+                    c = (f - 32) * 5 / 9;
+                    Console.WriteLine("Temperature in CELCIUS " + c);
+                    break;
+            }
+        }
+    }
+}
