bd953fba6a2f4267a1a7551efb799b57b8f09338
KajalShendage <shendagekajal123@gmail.com>
2022-08-29 22:59:50 +0530

    [Pramod] Add: Perfect Number

---
+++ LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PerfectNumber
+    {
+        public static void Perfectnumber()
+        {
+            int i;
+            int sum = 0;
+            int n;
+            Console.WriteLine("Enter any number to check whetner it is Perfect Number or not");
+            n = int.Parse(Console.ReadLine());
+            for (i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (n == sum)
+                Console.WriteLine("It is a Perfect Number");
+            else
+                Console.WriteLine("It is Not a Perfect Number");
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -2,12 +2,15 @@
 Console.WriteLine("Welcome to Logical Programming");
 while (true)
 {
-    Console.WriteLine("Select program\n 1. fibonacci series");
+    Console.WriteLine("Select program\n 1. fibonacci series\n 2. Perfect Number");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
         case 1:
             FibonacciSeries.Fibonacci();
+            break;
+        case 2:
+            PerfectNumber.Perfectnumber();
             break;
         default:
             Console.WriteLine("Select correct option");
